4286ca993f5add7ce5733b0298cb2ffe7c40b5b4
Mihaja <andriamiharisoamihaja@gmail.com>
2026-03-31 11:35:51 +0300

    Correction route

--- app/config/routes.php
+++ app/config/routes.php
@@ -13,17 +13,9 @@ $authController = new AuthController($app);
 $router->group('', function(Router $router) use ($authController, $app) {
 
     $router->get('/', function() use ($app) {
-        $app->render('auth/model.php', ['page' => 'home.php']);
+        $app->render('model.php', ['page' => 'home.php']);
     });
 
-    $router->get('/login', function() use ($app) {
-        $app->render('auth/model.php', ['page' => 'login.php']);
-    });
 
-    $router->get('/register', [$authController, 'showRegister']);
-
-    $router->post('/register', [$authController, 'postRegister']);
-
-    $router->post('/api/validate/register', [$authController, 'validateRegisterAjax']);
 
 }, [SecurityHeadersMiddleware::class]);
